ffafbcc535bf338f0d084f7d182098dde3f0468e
Yogesh Sunkra <yogeshsunkra123@gmail.com>
2024-07-01 22:06:13 +0530

    01/07

--- src/components/WeathertReport.js
+++ src/components/WeathertReport.js
@@ -1,32 +1,92 @@
 import React from 'react'
 import './weatherReport.css';
+import './utility.css';
 import { weatherList } from '../utility/dataUtlis';
 
 const WeathertReport = ({weatherInfo}) => {
 
   const weather = {};
 
-
+  const weatherCondition =(()=>{
+     weatherList.find((icon)=>icon.code === weather.code ? 
+    weather.icon =  weather.isDay ? icon.day : icon.night
+     :console.log("error")) ;
+  })
 
 
 
 
   if(weatherInfo){
 
+    // const weatherIcon ;
     weather.temp = weatherInfo.current.temp_c;
+    weather.code = weatherInfo.current.condition.code;
+    weather.isDay = weatherInfo.current.is_day;
+    weather.condition = weatherInfo.current.condition.text;
+    weather.feelsLike = weatherInfo.current.feelslike_c;
+    weather.visibility = weatherInfo.current.vis_km;
+    weather.windSpeed = weatherInfo.current.wind_kph;
+    weather.windDir = weatherInfo.current.wind_dir;
+    weather.windDeg = weatherInfo.current.wind_degree;
+    weather.uv = weatherInfo.current.uv;
+    weather.pressure = weatherInfo.current.pressure;
+    weather.humidity = weatherInfo.current.humidity;
+
+
+    weather.city = weatherInfo.location.name;
+    weather.country = weatherInfo.location.country;
+
+
+    weather.maxTemp = weatherInfo.forecast.forecastday[0].day.maxtemp_c;
+    weather.minTemp = weatherInfo.forecast.forecastday[0].day.mintemp_c;
+    weather.rainChances =  weatherInfo.forecast.forecastday[0].day.daily_chance_of_rain;
+
+    //Astro
+    weather.sunrise = weatherInfo.forecast.forecastday[0].astro.sunrise;
+    weather.sunset = weatherInfo.forecast.forecastday[0].astro.sunset;
+    weather.moonrise = weatherInfo.forecast.forecastday[0].astro.moonrise;
+    weather.moonset = weatherInfo.forecast.forecastday[0].astro.moonset;
     
-    console.log(weather.temp);
+    // console.log(weather.temp);
+
+
+    weatherCondition();
+
+
+
+    
+    // console.log("ICON" , icon);
   }
 
 
   return (
     <div className='weatherContainer'>
 
-<div className='weather'>
- {weather.temp};
+<div className='weather flex '>
+
+<div className='weatherHeader flex center'>
+
+<div className='location text-sm font-200 color-lt'>{weather.city + '/' + weather.country}</div>
+<div className='time text-sm font-200 color-lt'>04:50</div>
+
+</div>
+<div className='weatherBody flex center'>
+  <div className='weatherIcon '> {weather.icon}</div>
+  <div className='weatherTemp text-xl font-700 color'>{weather.temp}</div>
+  <div className='weatherCondition text-sm font-300 color-md'>{weather.condition} </div>
+</div>
+<div className='weatherFooter flex center'>
+  <div className='text-sm font-400 color-lt'>{weather.feelsLike}</div>
+  <div className='text-sm font-400 color-lt'>Something</div>
+</div>
+
+
  
  
-  {weatherList.map((icon)=> icon.day)}
+ 
+
+ 
+  
  
 
 
--- src/utility/dataUtlis.js
+++ src/utility/dataUtlis.js
@@ -51,6 +51,7 @@ import { ReactComponent as ThunderstormNightSnow } from "../assets/weatherIcons/
 import { ReactComponent as ThunderstormNight } from "../assets/weatherIcons/thunderstorms-night.svg";
 import { ReactComponent as ThunderstormRain } from "../assets/weatherIcons/thunderstorms-rain.svg";
 import { ReactComponent as ThunderstormSnow } from "../assets/weatherIcons/thunderstorms-snow.svg";
+import { IconBase } from "react-icons";
 
 
 
@@ -69,6 +70,297 @@ export const weekdays = [
 
 
 export const weatherList = [
+
+    {
+        code : 1000,
+        day : <ClearDay/>,
+        night : <ClearNight/>,
+
+    },
+    {
+        code : 1003,
+        day : <CloudyDay/>,
+        night : <CloudyNight/>,
+
+    },
+    {
+        code : 1006,
+        day : <Cloudy/>,
+        night : <Cloudy/>,
+
+    },
+    {
+        code : 1009,
+        day : <Overcast/>,
+        night : <Overcast/>,
+
+    },
+    {
+        code : 1030,
+        day : <Mist/>,
+        night : <Mist/>,
+
+    },
+    {
+        code : 1063,
+        day : <CloudyDayRain/>,
+        night : <CloudyNightRain/>,
+
+    },
+    {
+        code : 1066,
+        day : <CloudyDaySnow/>,
+        night : <CloudyNightSnow/>,
+
+    },
+    {
+        code : 1069,
+        day : <CloudyDaySleet/>,
+        night : <CloudyNightSleet/>,
+
+    },
+    {
+        code : 1072,
+        day : <CloudyDayDrizzle/>,
+        night : <CloudyNightDrizzle/>,
+
+    },
+    {
+        code : 1087,
+        day : <ThunderstormDay/>,
+        night : <ThunderstormNight/>,
+
+    },
+    {
+        code : 1114,
+        day : <CloudyDay/>, //##############
+        night : <CloudyNight/>,//##############
+
+    },
+    {
+        code : 1117,
+        day : <CloudyDay/>,//##############
+        night : <CloudyNight/>,//##############
+
+    },
+    {
+        code : 1135,
+        day : <FogDay/>,
+        night : <FogNight/>,
+
+    },
+    {
+        code : 1147,
+        day : <FogDay/>, 
+        night : <FogNight/>, 
+
+    },
+    {
+        code : 1150,
+        day : <CloudyDayDrizzle/>,
+        night : <CloudyNightDrizzle/>,
+
+    },
+    {
+        code : 1153,
+        day : <CloudyDayDrizzle/>,
+        night : <CloudyNightDrizzle/>,
+
+    },
+    {
+        code : 1168,
+        day : <Drizzle/>,
+        night : <Drizzle/>,
+
+    },
+    {
+        code : 1171,
+        day : <Drizzle/>,
+        night : <Drizzle/>,
+
+    },
+    {
+        code : 1180,
+        day : <CloudyDayRain/>,
+        night : <CloudyNightRain/>,
+
+    },
+    {
+        code : 1183,
+        day : <CloudyDayRain/>,
+        night : <CloudyNightRain/>,
+
+    },
+    {
+        code : 1186,
+        day : <CloudyDayRain/>,
+        night : <CloudyNightRain/>,
+
+    },
+    {
+        code : 1189,
+        day : <Rain/>,
+        night : <Rain/>,
+
+    },
+    {
+        code : 1192, 
+        day : <CloudyDayRain/>, 
+        night : <CloudyNightRain/>,
+
+    },
+    {
+        code : 1195,
+        day : <Rain/>, 
+        night : <Rain/>,
+
+    },
+    {
+        code : 1198,
+        day : <Rain/>,
+        night : <Rain/>,
+
+    },
+    {
+        code : 1201,
+        day : <Rain/>,
+        night : <Rain/>,
+
+    },
+    {
+        code : 1204,
+        day : <Sleet/>,
+        night : <Sleet/>,
+
+    },
+    {
+        code : 1207,
+        day : <Sleet/>,
+        night : <Sleet/>,
+
+    },
+    {
+        code : 1210,
+        day : <CloudyDaySnow/>,
+        night : <CloudyNightSnow/>,
+
+    },
+    {
+        code : 1213,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1216,
+        day : <CloudyDaySnow/>,
+        night : <CloudyNightSnow/>,
+
+    },
+    {
+        code : 1219,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1222,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1225,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1237,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1240,
+        day : <Rain/>,
+        night : <Rain/>,
+
+    },
+    {
+        code : 1243,
+        day : <Rain/>,
+        night : <Rain/>,
+
+    },
+    {
+        code : 1246,
+        day : <Rain/>,
+        night : <Rain/>,
+
+    },
+    {
+        code : 1249,
+        day : <Sleet/>,
+        night : <Sleet/>,
+
+    },
+    {
+        code : 1252,
+        day : <Sleet/>,
+        night : <Sleet/>,
+
+    },
+    {
+        code : 1255,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1258,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1261,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1264,
+        day : <Snow/>,
+        night : <Snow/>,
+
+    },
+    {
+        code : 1273,
+        day : <ThunderstormDayRain/>,
+        night : <ThunderstormNightRain/>,
+
+    },
+    {
+        code : 1276,
+        day : <ThunderstormRain/>,
+        night : <ThunderstormRain/>,
+
+    },
+    {
+        code : 1279,
+        day : <ThunderstormDaySnow/>,
+        night : <ThunderstormNightSnow/>,
+
+    },
+    {
+        code : 1282,
+        day : <ThunderstormSnow/>,
+        night : <ThunderstormSnow/>,
+
+    },
+
+    
 
     
 
